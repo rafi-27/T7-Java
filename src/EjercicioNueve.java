6e56da57138479f6812db5c510a7ee81c0a6fa7c
rafi-27 <rafikbachri21@gmail.com>
2023-12-07 12:55:44 +0100

    07/01/23

--- src/EjercicioNueve.java
+++ src/EjercicioNueve.java
@@ -3,16 +3,16 @@ import java.util.concurrent.ThreadLocalRandom;
 public class EjercicioNueve {
     public static void main(String[] args) {
         //Generar una contraseña desde la a-j y entre 5 y 10 caracteres.
-        String cadena = new String();
+        String cadena = "";
 
         String contenidoContrasenia = "abcdefghij";
-       //Generar la  cantidad de letras que va a tener nuestra contraseña.
+       //Generar la cantidad de letras que va a tener nuestra contraseña.
         int numeroAleatorio = ThreadLocalRandom.current().nextInt(5,10);
         //System.out.println(numeroAleatorio);
         //Iniciamos un bucle.
         for (int i = 0; i < numeroAleatorio; i++) {
             int numAC = ThreadLocalRandom.current().nextInt(contenidoContrasenia.length());
-            System.out.println(numAC);
+            //System.out.println(numAC);
             char contrasenia = contenidoContrasenia.charAt(numAC);
             cadena+=contrasenia;
         }
